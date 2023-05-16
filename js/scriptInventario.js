f396af2e925a1739862c8e2beb8c46470ee1a765
Leonardo Rodríguez Guzmán <s18120163@alumnos.itsur.edu.mx>
2023-05-15 19:48:38 -0600

    Trabajando funciones de crud

--- js/scriptInventario.js
+++ js/scriptInventario.js
@@ -10,18 +10,28 @@ $(document).ready(function() {
             {data:'nivel_reorden'},
             {data: null,
                 render: function(data, type, row) {
-                    return '<button class="btn btn-info">Editar</button> <button class="btn btn-danger">Eliminar</button>';
+                    return '<button class="btn btn-info" onclick="mostrarFormularioEditar()">Editar</button> <button class="btn btn-danger" onclick="eliminar()">Eliminar</button>';
                 }
             }
+            
         ],
         rowCallback: function(row, data, index) {
           if (data.nivel_reorden > data.existencia) {
               $(row).css('background-color', 'red');
           }
-      }
+        }
     });
+        // Agregar eventos a los botones
+        $('#miTabla tbody').on('click', '.editar', function () {
+          var data = miTabla.row($(this).parents('tr')).data();
+          alert('Editar: ' + data[0]);
+      });
+  
+      $('#miTabla tbody').on('click', '.eliminar', function () {
+          var data = miTabla.row($(this).parents('tr')).data();
+          alert('Eliminar: ' + data[0]);
+      });
 });
-
 
 var datos = 
 [
@@ -62,27 +72,51 @@ var datos =
     }
   ];
 
-  function agregar(){
+  function mostrarFormularioAgregar(){
+    let formularioContainer = document.getElementById("formulario-container");
+    let boton = document.getElementById("btn-agregar");
+    formularioContainer.style.display = "flex";
+    boton.style.display = "flex"
+    setTimeout(function() {
+      formularioContainer.style.transform = "translate(-50%, -50%) scale(1)";
+    }, 10);
+  };
 
-  }
+  function ocultarFormulario(){
+    let formularioContainer = document.getElementById("formulario-container");
+    let botonAgregar = document.getElementById("btn-agregar");
+    let botonEditar = document.getElementById("btn-editar");
+    formularioContainer.style.display = "flex";
+    botonAgregar.style.display = "none";
+    botonEditar.style.display = "none";
+    setTimeout(function() {
+      formularioContainer.style.transform = "translate(-50%, -50%) scale(0)";
+    }, 10);
+  };
 
-  function editar(){
+  //variables del formulario;
+  let clave = document.querySelector("clave");
+  let producto=document.querySelector("producto");
+  let categoria=document.querySelector("categoria");
+  let existencia=document.querySelector("existencia");
+  let nivel_reorden=document.querySelector("nivel_reorden");
+  //CRUD
+  function agregar(producto){
+    
+  };
 
-  }
+  function editar(clave){
 
-  function eliminar(){
+  };
 
-  }
+  function eliminar(clave){
+
+  };
+
+  function cancelar(){
+    ocultarFormulario();
+  };
+  
 
 
   
-    // Agregar eventos a los botones
-    $('#miTabla tbody').on('click', '.editar', function () {
-        var data = miTabla.row($(this).parents('tr')).data();
-        alert('Editar: ' + data[0]);
-    });
-
-    $('#miTabla tbody').on('click', '.eliminar', function () {
-        var data = miTabla.row($(this).parents('tr')).data();
-        alert('Eliminar: ' + data[0]);
-    });
