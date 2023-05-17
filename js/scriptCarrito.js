8d4edba38ac47553b6ad1aead2cef57fb237fcca
Vicente-Hernandez-Ballestros <s18120159@alumnos.itsur.edu.mx>
2023-05-17 00:22:02 -0600

    se modifico el carrito de compra

--- js/scriptCarrito.js
+++ js/scriptCarrito.js
@@ -1,125 +1,113 @@
-var productos = [
-    { producto: 'Laptop HP', categoria: 'Electrónicos', precio: 10000, existencia: 10, foto: 'ruta/foto1.jpg' },
-    { producto: 'Mouse Logitech', categoria: 'Electrónicos', precio: 500, existencia: 3, foto: 'ruta/foto2.jpg' },
-    { producto: 'Impresora Epson', categoria: 'Electrónicos', precio: 2000, existencia: 0, foto: 'ruta/foto3.jpg' }
+
+// Datos del arreglo
+const datos = [
+  {
+    producto: 'Laptop Hp',
+    categoria: 'Electronicos',
+    precio: 10000,
+    existencia: 5
+  },
+  {
+    producto: 'Impresora',
+    categoria: 'Electronicos',
+    precio: 2000,
+    existencia: 8
+  },
+  {
+    producto: 'Mouse',
+    categoria: 'Electronicos',
+    precio: 1500,
+    existencia: 8
+  },
+  {
+    producto: 'Bolso',
+    categoria: 'Moda',
+    precio: 5000,
+    existencia: 3
+  }
+  // Agrega más objetos según tus necesidades
 ];
 
-var carrito = [];
+// Mapeo de productos a imágenes
+const imagenes = {
+  'Laptop Hp': 'imagenes/laptop-hp.jpg',
+  'Impresora': 'imagenes/impresora-hp.jpg',
+  'Mouse': 'imagenes/mouse-logitec.jpg',
+  'Bolso':'imagenes/bolso.jpg'
+  // Agrega más productos e imágenes según corresponda
+};
 
-// Función para mostrar los productos en la lista
-function mostrarProductos() {
-    limpiarTablaProductos();
-    var tabla = $('#tabla-productos').DataTable();
+const cardContainer = document.getElementById('cardContainer');
 
-    productos.forEach(function (producto) {
-        tabla.row.add([
-            producto.producto,
-            producto.categoria,
-            producto.precio,
-            producto.existencia,
-            '<button class="btn btn-primary btn-carrito" onclick="agregarAlCarrito(\'' + producto.producto + '\')">Agregar al Carrito</button>'
-        ]).draw(false);
-    });
+
+
+// Recorrer los datos y crear las tarjetas dinámicamente
+datos.forEach(item => {
+  const card = document.createElement('div');
+  card.classList.add('col');
+
+  card.innerHTML = `
+  <div class="card text-center text-white bg-primary my-card " style="width: 13rem;">
+      <img src="${imagenes[item.producto]}" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">${item.producto}</h5>
+          <p class="card-text">${item.categoria}</p>
+          <p class="card-text">$${item.precio.toFixed(2)}</p>
+          <p class="card-text">${item.existencia} unidades</p>
+          <a href="#" class="btn btn-light">Agregar al carrito</a>
+      </div>
+  </div>
+`;
+
+  cardContainer.appendChild(card);
+});
+
+
+// Seleccione el contenedor de elementos del carrito
+const cartItemsContainer = document.getElementById('cartItems');
+
+// Función para agregar un artículo al carrito
+function addToCart(product) {
+  // Crear una nueva fila para el artículo
+  const row = document.createElement('tr');
+
+  // Establecer el contenido HTML de la fila
+  row.innerHTML = `
+    <td><img src="${imagenes[product.producto]}" alt="${product.producto}" width="50"></td>
+    <td>${product.producto}</td>
+    <td>$${product.precio.toFixed(2)}</td>
+    <td>1</td>
+  `;
+
+  // Agregue la fila al contenedor de elementos del carrito
+  cartItemsContainer.appendChild(row);
+
+  // Actualizar el precio total
+  updateTotal(product.precio);
 }
 
-function limpiarTablaProductos() {
-    var tabla = $('#tabla-productos').DataTable();
-    tabla.clear().draw();
+// Actualizar el precio total
+function updateTotal(price) {
+  const cartTotalElement = document.getElementById('cartTotal');
+  const cartTotal = parseFloat(cartTotalElement.textContent.slice(1)); // Extract the numerical value
+
+  // Calcular el nuevo total
+  const newTotal = cartTotal + price;
+
+  // Actualizar el precio total en el HTML
+  cartTotalElement.textContent = `$${newTotal.toFixed(2)}`;
 }
 
+// Creacion de eventos para manejar los clics del botón Agregar al carrito
+cardContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('btn')) {
+    const card = event.target.closest('.card');
+    const productName = card.querySelector('.card-title').textContent;
+    const product = datos.find(item => item.producto === productName);
 
-// Función para agregar un producto al carrito
-function agregarAlCarrito(producto) {
-    var productoEncontrado = productos.find(function (p) {
-      return p.producto === producto;
-    });
-  
-    if (productoEncontrado) {
-      if (productoEncontrado.existencia > 0) {
-        var productoEnCarrito = carrito.find(function (item) {
-          return item.producto === productoEncontrado.producto;
-        });
-  
-        if (productoEnCarrito) {
-          productoEnCarrito.unidades++;
-        } else {
-          carrito.push({ ...productoEncontrado, unidades: 1 });
-        }
-  
-        productoEncontrado.existencia--;
-  
-        var tablaCarrito = $('#tabla-carrito').DataTable();
-        tablaCarrito.clear().draw();
-  
-        carrito.forEach(function (item) {
-          tablaCarrito.row.add([
-            item.producto,
-            '<img src="' + item.foto + '" width="50" height="50">',
-            item.precio,
-            item.unidades,
-            '<button class="btn btn-danger" onclick="eliminarDelCarrito(\'' + item.producto + '\')">Eliminar</button>'
-          ]).draw(false);
-        });
-  
-        mostrarProductos();
-      } else {
-        alert('No hay existencia disponible para este producto.');
-      }
+    if (product && product.existencia > 0) {
+      addToCart(product);
+      product.existencia--; 
     }
   }
-
-
-// Función para eliminar un producto del carrito
-function eliminarDelCarrito(producto) {
-    var index = carrito.findIndex(function (item) {
-      return item.producto === producto;
-    });
-  
-    if (index !== -1) {
-      var productoEnCarrito = carrito[index];
-  
-      if (productoEnCarrito.unidades > 1) {
-        productoEnCarrito.unidades--;
-      } else {
-        carrito.splice(index, 1);
-      }
-  
-      var tablaCarrito = $('#tabla-carrito').DataTable();
-      tablaCarrito.clear().draw();
-  
-      carrito.forEach(function (item) {
-        tablaCarrito.row.add([
-          item.producto,
-          '<img src="' + item.foto + '" width="50" height="50">',
-          item.precio,
-          item.unidades,
-          '<button class="btn btn-danger" onclick="eliminarDelCarrito(\'' + item.producto + '\')">Eliminar</button>'
-        ]).draw(false);
-      });
-  
-      // Restablecer unidades en la lista de productos
-      var productoEncontrado = productos.find(function (p) {
-        return p.producto === producto;
-      });
-  
-      if (productoEncontrado) {
-        productoEncontrado.existencia++;
-        mostrarProductos();
-      }
-    }
-  }   
-
-
-// Función para confirmar la compra
-function confirmarCompra() {
-    // Lógica para confirmar la compra
-}
-
-$(document).ready(function () {
-    // Inicializar DataTables
-    $('#tabla-productos').DataTable();
-    $('#tabla-carrito').DataTable();
-
-    // Mostrar los productos en la lista
-    mostrarProductos();
 });
