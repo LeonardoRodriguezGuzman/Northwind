f7f7c65b1ba1ebc12d80ef010c44a6925ac7b2bc
LeonardoRodriguezGuzman <s18120163@alumnos.itsur.edu.mx>
2023-05-17 00:12:10 -0600

    Responsivo funcional

--- js/scriptInventario.js
+++ js/scriptInventario.js
@@ -1,40 +1,45 @@
-$(document).ready(function() {
-    $('#tabla').DataTable({
-        responsive: true,
-        data: datos,
-        columns: [
-            {data:'clave'},
-            {data:'producto'},
-            {data:'categoria'},
-            {data:'existencia'},
-            {data:'nivel_reorden'},
-            {data: null,
-                render: function(data, type, row) {
-                    return '<button class="btn btn-info" onclick="mostrarFormularioEditar()">Editar</button> <button class="btn btn-danger" onclick="eliminar()">Eliminar</button>';
-                }
-            }
-            
-        ],
-        rowCallback: function(row, data, index) {
-          if (data.nivel_reorden > data.existencia) {
-              $(row).css('background-color', 'red');
-          }
+$(document).ready(function () {
+  $('#tabla').DataTable({
+    responsive: {
+      breakpoints: [
+        { name: 'phone', width: 600 }
+      ]
+    },
+    data: datos,
+    columns: [
+      { data: 'clave', responsivePriority: 3 },
+      { data: 'producto', responsivePriority: 1 },
+      { data: 'categoria', responsivePriority: 4 },
+      { data: 'existencia', responsivePriority: 2 },
+      { data: 'nivel_reorden' },
+      {
+        data: null,
+        responsivePriority: 5,
+        render: function (data, type, row) {
+          return '<button class="btn btn-info" onclick="mostrarFormularioEditar()">Editar</button> <button class="btn btn-danger" onclick="eliminar()">Eliminar</button>';
         }
-    });
-        // Agregar eventos a los botones
-        $('#miTabla tbody').on('click', '.editar', function () {
-          var data = miTabla.row($(this).parents('tr')).data();
-          alert('Editar: ' + data[0]);
-      });
-  
-      $('#miTabla tbody').on('click', '.eliminar', function () {
-          var data = miTabla.row($(this).parents('tr')).data();
-          alert('Eliminar: ' + data[0]);
-      });
+      }
+    ],
+    rowCallback: function (row, data, index) {
+      if (data.nivel_reorden > data.existencia) {
+        $(row).css('background-color', 'red');
+      }
+    }
+  });
+  // Agregar eventos a los botones
+  $('#miTabla tbody').on('click', '.editar', function () {
+    var data = miTabla.row($(this).parents('tr')).data();
+    alert('Editar: ' + data[0]);
+  });
+
+  $('#miTabla tbody').on('click', '.eliminar', function () {
+    var data = miTabla.row($(this).parents('tr')).data();
+    alert('Eliminar: ' + data[0]);
+  });
 });
 
-var datos = 
-[
+var datos =
+  [
     {
       'clave': "PROD001",
       'producto': "Laptop HP",
@@ -72,51 +77,59 @@ var datos =
     }
   ];
 
-  function mostrarFormularioAgregar(){
-    let formularioContainer = document.getElementById("formulario-container");
-    let boton = document.getElementById("btn-agregar");
-    formularioContainer.style.display = "flex";
-    boton.style.display = "flex"
-    setTimeout(function() {
-      formularioContainer.style.transform = "translate(-50%, -50%) scale(1)";
-    }, 10);
-  };
+function mostrarFormularioAgregar() {
+  let formularioContainer = document.getElementById("formulario-container");
+  let boton = document.getElementById("btn-agregar");
+  formularioContainer.style.display = "flex";
+  boton.style.display = "flex"
+  setTimeout(function () {
+    formularioContainer.style.transform = "translate(-50%, -50%) scale(1)";
+  }, 10);
+};
 
-  function ocultarFormulario(){
-    let formularioContainer = document.getElementById("formulario-container");
-    let botonAgregar = document.getElementById("btn-agregar");
-    let botonEditar = document.getElementById("btn-editar");
-    formularioContainer.style.display = "flex";
-    botonAgregar.style.display = "none";
-    botonEditar.style.display = "none";
-    setTimeout(function() {
-      formularioContainer.style.transform = "translate(-50%, -50%) scale(0)";
-    }, 10);
-  };
+function mostrarFormularioEditar() {
+  let formularioContainer = document.getElementById("formulario-container");
+  let boton = document.getElementById("btn-editar");
+  formularioContainer.style.display = "flex";
+  boton.style.display = "flex"
+  setTimeout(function () {
+    formularioContainer.style.transform = "translate(-50%, -50%) scale(1)";
+  }, 10);
+};
+function ocultarFormulario() {
+  let formularioContainer = document.getElementById("formulario-container");
+  let botonAgregar = document.getElementById("btn-agregar");
+  let botonEditar = document.getElementById("btn-editar");
+  formularioContainer.style.display = "flex";
+  botonAgregar.style.display = "none";
+  botonEditar.style.display = "none";
+  setTimeout(function () {
+    formularioContainer.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 10);
+};
 
-  //variables del formulario;
-  let clave = document.querySelector("clave");
-  let producto=document.querySelector("producto");
-  let categoria=document.querySelector("categoria");
-  let existencia=document.querySelector("existencia");
-  let nivel_reorden=document.querySelector("nivel_reorden");
-  //CRUD
-  function agregar(producto){
-    
-  };
+//variables del formulario;
+let clave = document.querySelector("clave");
+let producto = document.querySelector("producto");
+let categoria = document.querySelector("categoria");
+let existencia = document.querySelector("existencia");
+let nivel_reorden = document.querySelector("nivel_reorden");
+//CRUD
+function agregar(producto) {
 
-  function editar(clave){
+};
 
-  };
+function editar(clave) {
 
-  function eliminar(clave){
+};
 
-  };
+function eliminar(clave) {
 
-  function cancelar(){
-    ocultarFormulario();
-  };
-  
+};
+
+function cancelar() {
+  ocultarFormulario();
+};
 
 
-  
+
